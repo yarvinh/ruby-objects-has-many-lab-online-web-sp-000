f16e5faceaf79fe82476e6f2c2067851f2a2dd1b
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-26 01:12:23 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,7 +8,7 @@ class Author
     Post.all.select {|post| post.author == self}
   end
   def add_post(post)
-    post.author #= self
+    post.author = self
   end
   def add_post_by_title(title)
     post = Post.new(title)
